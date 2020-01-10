27d36561f4112de5afb3a7ee9e7e7bb4af35fc15
SkyNokK <jean.ferro@ynov.com>
2020-01-10 17:37:32 +0100

    Ajout id pour suppresion

--- Gantt/Client/gantt.js
+++ Gantt/Client/gantt.js
@@ -2,7 +2,7 @@ const socket = io();
 
 socket.emit("connection", "connected");
 
-$("#addTask").submit(function(e) {
+$("#addTask").submit(function (e) {
   e.preventDefault();
   socket.emit("name", $("#name").val());
   socket.emit("desc", $("#desc").val());
@@ -19,5 +19,8 @@ socket.on("desc", data => $("#gantt_desc").append($("<li>").text(data)));
 socket.on("task", data =>
   $("#gantt_task")
     .append($("<li>").text(data))
-    .append($("</li><button>Supprimer</button>"))
+);
+socket.on("taskId", data =>
+  $("#gantt_task")
+    .append($("</li><button id=\"" + data + "\">Supprimer</button>"))
 );
