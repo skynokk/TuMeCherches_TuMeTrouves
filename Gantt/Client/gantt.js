ce5fdc06d02d8943c8f517c3ef6939614b80961a
venomania <bruno.martinsvilasboas@ynov.com>
2020-01-13 08:23:40 +0100

    css +++

--- Gantt/Client/gantt.js
+++ Gantt/Client/gantt.js
@@ -13,6 +13,7 @@ $("#addTask").submit(function (e) {
   socket.emit("color", $("#color").val());
   socket.emit("linkedTask", $("#linkedTask").val());
   socket.emit("ressources", $("#ressources").val());
+  document.location.reload(true);
   return false;
 });
 
@@ -20,12 +21,14 @@ $("#suppressionTache").submit(function (e) {
   e.preventDefault();
   let suppressionId = $("input[name='tache']:checked").val();
   socket.emit("suppressionId", suppressionId);
+  document.location.reload(true);
 });
 
 $("#modificationTache").submit(function (e) {
   e.preventDefault();
   let modificationId = $("input[name='tache']:checked").val();
   socket.emit("modificationId", modificationId);
+  document.location.reload(true);
 });
 
 socket.on("desc", data => $("#gantt_desc").append($("<li>").text(data)));
@@ -42,7 +45,13 @@ socket.on("task", data =>
 
 socket.on("taskProject", data =>
   $("#gantt_project")
-    .append($("<li>").text(data))
+
+    .append($("<li class=\"text-chacher\">").text(chars = data.split(",")))
+    .append($("<p class=\"date\">").text("12/05/2020-15h30-----------15/05/2020-15h30"))
+    .append($("<li>").text(chars[0]))
+    .append($("<li class=\"progress-bar\" role=\"progressbar\" style=\"max-width: 97%; width:"+chars[2]+"%;color: white; position: absolute;background-color:"+chars[3]+";\" aria-valuenow="+chars[2]+"; aria-valuemin=\"0\" aria-valuemax=\"100\">").text(chars[2]+"%/100%"))
+    .append($("<li class=\"progress-bar\" role=\"progressbar\" style=\"width: 100%;background-color: gray;color: gray;\" aria-valuenow=\"25\" aria-valuemin=\"0\" aria-valuemax=\"100\">").text("..."))
+
 );
 
 /*
